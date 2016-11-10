7bfd3cc716c38e424bc752b5786f24f8d490525e
chel1982 <chel@activist.com>
2016-11-10 12:31:11 +0200

    add fixture

---
+++ src/A2x/BlogBundle/DataFixtures/ORM/LoadPostData.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace A2x\BlogBundle\DataFixtures\ORM;
+
+use A2x\BlogBundle\Entity\Post;
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class LoadPostData implements FixtureInterface
+{
+    public function load(ObjectManager $manager)
+    {
+        $post = new Post();
+        $post->setTitle('Книга по PHP');
+        $post->setDescription('Учебник по PHP');
+        $post->setDescription('Lorem');
+
+        $manager->persist($post);
+        $manager->flush();
+    }
+
+    public function getOrder()
+    {
+        // the order in which fixtures will be loaded
+        // the lower the number, the sooner that this fixture is loaded
+        return 1;
+    }
+}
